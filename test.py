134b2ed08f4a5f947dac67876ea9be473e467439
jarrodschilling <jarrodschilling@gmail.com>
2023-11-04 08:33:23 -0600

    yfinance ticker issues

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import yfinance as yf
 
-msft = yf.Ticker("MSFT")
-msft.info
+# msft = yf.Ticker("MSFT")
+# msft.info
+
+def api_historical_data(symbols):
+    data = yf.download(symbols, start="2022-01-01", end="2023-10-18")
+    return data['Close']
+
+print(api_historical_data("jpm"))
