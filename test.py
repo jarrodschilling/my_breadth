606191934f61f84207ab0109102db66f3baf201f
jarrodschilling <jarrodschilling@gmail.com>
2023-11-18 12:49:02 -0700

    OOP incoming

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import yfinance as yf
 
-# msft = yf.Ticker("MSFT")
-# msft.info
+msft = yf.Ticker("MSFT")
+print(msft.info)
 
-def api_historical_data(symbols):
-    data = yf.download(symbols, start="2022-01-01", end="2023-10-18")
-    return data['Close']
 
-print(api_historical_data("jpm"))
+# def api_historical_data(symbols):
+#     data = yf.download(symbols, start="2022-01-01", end="2023-10-18")
+#     return data['Close']
+
+# print(api_historical_data("jpm"))
